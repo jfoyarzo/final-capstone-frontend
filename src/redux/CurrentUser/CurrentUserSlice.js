76a469a9302179f5a01bd2f61987b026fcea8cfe
jfoyarzo <foyarzo621@gmail.com>
2023-03-17 19:04:19 -0300

    Add currentUserSlice and update store

---
+++ src/redux/CurrentUser/CurrentUserSlice.js
@@ -0,0 +1,32 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = [];
+const url = 'http://p-i-api.onrender.com/users/sign_in';
+
+export const getUser = createAsyncThunk('currentUser/getUser', async (user) => {
+  const response = await axios.post(url, {
+    user: {
+      email: user.email,
+      password: user.password,
+    },
+  }, { withCredentials: true });
+  console.log(response);
+  return response.data.data;
+});
+
+const currentUserSlice = createSlice({
+  name: 'currentUser',
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(getUser.fulfilled, (state, action) => {
+      const currentUser = action.payload;
+      return currentUser;
+    });
+    builder.addCase(getUser.rejected, (state, action) => {
+      console.log(action.error);
+    });
+  },
+});
+
+export default currentUserSlice.reducer;

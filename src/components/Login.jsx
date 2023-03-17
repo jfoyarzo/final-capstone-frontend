f44c50512576b2edfda09029cd2f3e31d372f5d6
jfoyarzo <foyarzo621@gmail.com>
2023-03-17 19:04:44 -0300

    Add login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { getUser } from '../redux/CurrentUser/CurrentUserSlice';
 
-const Login = () => (<h1><Login /></h1>);
+const Login = () => {
+  const dispatch = useDispatch();
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const [email, password] = document.getElementById('form').elements;
+    console.log(email.value);
+    console.log(password.value);
+    dispatch(getUser({ email: email.value, password: password.value }));
+  };
+  return (
+    <form id="form" onSubmit={handleLogin}>
+      <label htmlFor="email">
+        Email
+        <input type="email" name="email" />
+      </label>
+      <label htmlFor="password">
+        Password
+        <input type="password" htmlFor="password" />
+      </label>
+      <button type="submit" onClick={handleLogin}>
+        Login
+      </button>
+    </form>
+  );
+};
 
 export default Login;

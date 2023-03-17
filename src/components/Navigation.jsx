7db38fba15e701f3272999403307ea8ba88c165d
iAmWaris97 <haleem.waris@gmail.com>
2023-03-17 18:37:21 +0430

    Add navigation component

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Navigation = () => (<h1>Navbar</h1>);
+// eslint-disable-next-line no-unused-vars, arrow-body-style
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/add-investigator">Add New Investigator</Link>
+        </li>
+        <li>
+          <Link to="/my-reservations">My Reservations</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navigation;

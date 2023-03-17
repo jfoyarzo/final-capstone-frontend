4142f68986996dff6585895b82aa77c916cfa073
emmiiorji <emmanuelorji2013@gmail.com>
2023-03-17 22:34:05 +0100

    Update investigators component

--- src/components/investigators/Investigators.jsx
+++ src/components/investigators/Investigators.jsx
@@ -1,29 +1,16 @@
-// import { nanoid } from '@reduxjs/toolkit';
-// import { useSelector } from 'react-redux';
 import React from 'react';
-import { BiLeftArrow, BiRightArrow } from 'react-icons/bi';
-// import InvestigatorCard from './InvestigatorCard';
 import InvestigatorsSlider from './InvestigatorsSlider';
 import './Investigators.css';
 import SideBar from './SideBar';
 import './SideBar.css';
 
 const Investigators = () => (
-  // const investigators = useSelector((state) => state.investigators);
-  <div>
+  <div id="investigators-main-container">
     <SideBar />
-    <div>
+    <div id="investigators-container">
       <h1>Latest Investigators</h1>
       <p>Please select an investigator</p>
-      <div className="slide-container">
-        <div className="arrow-container">
-          <BiLeftArrow className="arrow" />
-        </div>
-        <InvestigatorsSlider />
-        <div className="arrow-container">
-          <BiRightArrow className="arrow" />
-        </div>
-      </div>
+      <InvestigatorsSlider />
     </div>
   </div>
 );

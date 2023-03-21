d2b6b0c7a5f5b49ed08cc700dc73b79d136ad60f
iAmWaris97 <haleem.waris@gmail.com>
2023-03-22 03:45:04 +0430

    map investigator conditionaly in select element in appoinment page

--- src/components/investigators/Investigators.jsx
+++ src/components/investigators/Investigators.jsx
@@ -1,23 +1,15 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import React from 'react';
 import InvestigatorsSlider from './InvestigatorsSlider';
-import { fetchInvestigators } from '../../redux/investigators/investigatorSlice';
 import './Investigators.css';
 
-const Investigators = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchInvestigators());
-  }, [dispatch]);
-  return (
-    <div id="investigators-main-container">
-      <div id="investigators-container">
-        <h1>Latest Investigators</h1>
-        <p>Please select an investigator</p>
-        <InvestigatorsSlider />
-      </div>
+const Investigators = () => (
+  <div id="investigators-main-container">
+    <div id="investigators-container">
+      <h1>Latest Investigators</h1>
+      <p>Please select an investigator</p>
+      <InvestigatorsSlider />
     </div>
-  );
-};
+  </div>
+);
 
 export default Investigators;

f68432a96d8c3a12d8eea3df06af013c277a067a
jfoyarzo <foyarzo621@gmail.com>
2023-03-21 16:42:11 -0300

    Update investigators component with api fetch

--- src/components/investigators/Investigators.jsx
+++ src/components/investigators/Investigators.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import InvestigatorsSlider from './InvestigatorsSlider';
+import { fetchInvestigators } from '../../redux/investigators/investigatorSlice';
 import './Investigators.css';
 
-const Investigators = () => (
-  <div id="investigators-main-container">
-    <div id="investigators-container">
-      <h1>Latest Investigators</h1>
-      <p>Please select an investigator</p>
-      <InvestigatorsSlider />
+const Investigators = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchInvestigators());
+  }, [dispatch]);
+  return (
+    <div id="investigators-main-container">
+      <div id="investigators-container">
+        <h1>Latest Investigators</h1>
+        <p>Please select an investigator</p>
+        <InvestigatorsSlider />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Investigators;

8758548f4d1e06996f73c050a3b572296d7862da
iAmWaris97 <haleem.waris@gmail.com>
2023-03-22 03:44:26 +0430

    move file to coresponding directory

--- src/components/appointments/ReserveForm.jsx
+++ src/components/appointments/ReserveForm.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { createAppointment } from '../redux/Appointments/appointmentsSlice';
+import { nanoid } from '@reduxjs/toolkit';
+import { createAppointment } from '../../redux/Appointments/appointmentsSlice';
 
 const ReserveForm = () => {
   const { id } = useParams();
+  console.log(id);
   const [state, setState] = useState({ date: '', text: '' });
   const dispatch = useDispatch();
 
+  const currentUser = useSelector((state) => state.userReducer);
   const handleChange = (event, name) => {
     if (name === 'date' || name === 'text') {
       setState((state) => ({
@@ -24,18 +27,30 @@ const ReserveForm = () => {
     }
   };
 
+  const investigators = useSelector((state) => state.investigators.value);
   return (
     <div>
       <h1>Make an appointment</h1>
       <hr />
       <p>Our investigators are amazing, you need to book one now!!!</p>
       <form onSubmit={handleSubmit}>
+        <input type="text" value={currentUser.name} required disabled />
         <input type="date" value={state.date} required onChange={(e) => handleChange(e, 'date')} />
         <input type="text" value={state.text} required onChange={(e) => handleChange(e, 'text')} />
+        {investigators.map((i) => {
+          if (i.id === id) {
+            return (
+              <option key={nanoid()} value="volvo">{i.name}</option>
+            );
+          }
+          return (
+            <option key={nanoid()} value="volvo">{i.name}</option>
+          );
+        })}
+        ;
         <button type="submit">Submit</button>
       </form>
     </div>
-
   );
 };
 

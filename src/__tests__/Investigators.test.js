33d5547179ca54a0e2a4ea2c7de37eb224a47252
iAmWaris97 <haleem.waris@gmail.com>
2023-03-22 18:07:04 +0430

    add test cases for the investigators index page

--- src/__tests__/Investigators.test.js
+++ src/__tests__/Investigators.test.js
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import renderer from 'react-test-renderer';
-// import { screen, waitFor } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
 import Investigators from '../components/investigators/Investigators';
@@ -18,5 +18,23 @@ describe('To test investigators index page!', () => {
         </Provider>,
       )
       .toJSON();
+  });
+
+  it('Component should render correctly', () => {
+    expect(investigators).toBeTruthy();
+  });
+
+  it('It should match the snapshot', () => {
+    expect(investigators).toMatchSnapshot();
+  });
+
+  it('Should render the component', () => {
+    waitFor(() => expect(screen.getByText('Latest Investigators')).toBeInTheDocument());
+    waitFor(() => expect(screen.getByText('Please select an investigator')).toBeInTheDocument());
+  });
+
+  it('Should link investigator to their facebook page', () => {
+    const link = screen.findByTestId('investigator-link');
+    waitFor(() => expect(link).toBeInTheDocument());
   });
 });

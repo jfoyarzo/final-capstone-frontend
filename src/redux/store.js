65d53eb15f6721883992c979de34c23100ab812f
iAmWaris97 <haleem.waris@gmail.com>
2023-03-22 21:06:46 +0430

    add investigatore reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import investigatorsReducer from './investigators/investigatorSlice';
 import appointmentsReducer from './Appointments/appointmentsSlice';
 import userReducer from './CurrentUser/CurrentUserSlice';
+import addInvestigatorsReducer from './InvestigatorForm/investigatorFormSlice';
 
 const store = configureStore({
   reducer: {
     investigators: investigatorsReducer,
     appointments: appointmentsReducer,
+    addInvestigatorsReducer,
     userReducer,
   },
 });

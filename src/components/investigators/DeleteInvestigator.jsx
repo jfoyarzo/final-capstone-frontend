659c2aea489b3563a10444118c560db4363a8e5b
Chris Clothier <c.r.clothier@gmail.com>
2023-03-22 21:06:31 +0000

    Implement deleteInvestigator

--- src/components/investigators/DeleteInvestigator.jsx
+++ src/components/investigators/DeleteInvestigator.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteInvestigator } from '../../redux/DeleteInvestigator/deleteInvestigatorSlice';
+import { Button, Card } from 'react-bootstrap';
 
-const DeleteInvestigator = () => (<h1>Delete Form</h1>);
+const DeleteInvestigator = () => (
+  <div>
+    <h1>Delete an Investigator</h1>
+    <div>
+      {investigators.value.map((investigator) => (
+          <Card key={investigator.id} className="d-flex flex-row justify-content-between m-4 p-4">
+            <img src={`${investigator.photo}`} alt="photo" style={{ maxHeight: "2rem" }} />
+            {investigator.name}
+            <Button
+              type="button"
+              id={investigator.id}
+              onClick={handleDelete}
+            >
+              Delete
+            </Button>
+          </Card>
+        ))}
+      </div>
+  </div>
+  );
 
 export default DeleteInvestigator;

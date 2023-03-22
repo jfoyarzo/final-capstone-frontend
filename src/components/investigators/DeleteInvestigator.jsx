5bb57283b48b5296b51df72fa7b4c3ed21fde910
Chris Clothier <c.r.clothier@gmail.com>
2023-03-22 21:07:03 +0000

    Add dispatch to delete investigator

--- src/components/investigators/DeleteInvestigator.jsx
+++ src/components/investigators/DeleteInvestigator.jsx
@@ -3,13 +3,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import { deleteInvestigator } from '../../redux/DeleteInvestigator/deleteInvestigatorSlice';
 import { Button, Card } from 'react-bootstrap';
 
-const DeleteInvestigator = () => (
-  <div>
-    <h1>Delete an Investigator</h1>
+const DeleteInvestigator = () => {
+  const investigators = useSelector((state) => state.investigators);
+
+  const dispatch = useDispatch();
+
+  return (
     <div>
-      {investigators.value.map((investigator) => (
+      <h1>Delete an Investigator</h1>
+      <div>
+        {investigators.value.map((investigator) => (
           <Card key={investigator.id} className="d-flex flex-row justify-content-between m-4 p-4">
-            <img src={`${investigator.photo}`} alt="photo" style={{ maxHeight: "2rem" }} />
+            <img src={`${investigator.photo}`} alt="photo" style={{maxHeight: "2rem"}} />
             {investigator.name}
             <Button
               type="button"
@@ -21,7 +26,8 @@ const DeleteInvestigator = () => (
           </Card>
         ))}
       </div>
-  </div>
+    </div>
   );
+};
 
 export default DeleteInvestigator;

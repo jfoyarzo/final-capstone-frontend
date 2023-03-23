804a0cdc72979e37c9d059399ba9a5fd43c8ecd7
emmiiorji <emmanuelorji2013@gmail.com>
2023-03-23 18:36:31 +0100

    Fix appointments render after creating appointment

--- src/components/appointments/Appointments.jsx
+++ src/components/appointments/Appointments.jsx
@@ -2,19 +2,21 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { nanoid } from '@reduxjs/toolkit';
 import { deleteAppointment, fetchAppointments } from '../../redux/Appointments/appointmentsSlice';
+import { fetchInvestigators } from '../../redux/investigators/investigatorSlice';
 
 const Appointments = () => {
+  const appointments = useSelector((state) => state.appointments);
+  const investigators = useSelector((state) => state.investigators);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchAppointments());
+    dispatch(fetchInvestigators());
   }, [dispatch]);
 
   const handleDelete = (event) => {
     dispatch(deleteAppointment(event.target.id));
   };
 
-  const appointments = useSelector((state) => state.appointments);
-  const investigators = useSelector((state) => state.investigators.value);
   return (
     <div>
       <h1>Appointments</h1>
@@ -27,19 +29,24 @@ const Appointments = () => {
             <th>Action</th>
           </tr>
         </thead>
+        {
+          investigators.status === 'fetched'
+        && (
         <tbody>
-          {appointments.value.map((appointment) => {
-            const investigator = investigators.find(({ id }) => id === appointment.investigator_id);
-            return (
-              <tr key={nanoid()}>
-                <td>{investigator.name}</td>
-                <td>{appointment.city}</td>
-                <td>{appointment.date}</td>
-                <td><button type="button" onClick={handleDelete} id={appointment.id}>Cancel</button></td>
-              </tr>
-            );
-          })}
+          {appointments.value.map((appointment) => (
+            <tr key={nanoid()}>
+              <td>
+                {investigators.value.find(({ id }) => id === appointment.investigator_id).name}
+              </td>
+              <td>{appointment.city}</td>
+              <td>{appointment.date}</td>
+              <td><button type="button" onClick={handleDelete} id={appointment.id}>Cancel</button></td>
+            </tr>
+          ))}
         </tbody>
+        )
+
+        }
       </table>
     </div>
   );

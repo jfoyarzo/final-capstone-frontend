a0204a5449f5d1c0840e294a7d44d1441dd2c85e
iAmWaris97 <haleem.waris@gmail.com>
2023-03-23 21:16:53 +0430

    Update routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Investigator from './components/investigators/Investigator';
 import Welcome from './components/Welcome';
 import Signup from './components/Signup';
@@ -14,28 +14,26 @@ import Appointments from './components/appointments/Appointments';
 import ProtectedRoute from './utils/ProtectedRoute';
 
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Welcome />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route
-        path="/app/"
-        element={(
-          <ProtectedRoute>
-            <Layout />
-          </ProtectedRoute>
+  <Routes>
+    <Route path="/" element={<Welcome />} />
+    <Route path="/signup" element={<Signup />} />
+    <Route path="/login" element={<Login />} />
+    <Route
+      path="/app/"
+      element={(
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
       )}
-      >
-        <Route path="investigators" element={<Investigators />} />
-        <Route path="investigators/:id" element={<Investigator />} />
-        <Route path="add_investigator" element={<InvestigatorForm />} />
-        <Route path="investigators/:id/reserve" element={<ReserveForm />} />
-        <Route path="delete-investigator/:id" element={<DeleteInvestigator />} />
-        <Route path="appointments" element={<Appointments />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+    >
+      <Route path="investigators" element={<Investigators />} />
+      <Route path="investigators/:id" element={<Investigator />} />
+      <Route path="add_investigator" element={<InvestigatorForm />} />
+      <Route path="investigators/:id/reserve" element={<ReserveForm />} />
+      <Route path="delete-investigator/:id" element={<DeleteInvestigator />} />
+      <Route path="appointments" element={<Appointments />} />
+    </Route>
+  </Routes>
 );
 
 export default App;

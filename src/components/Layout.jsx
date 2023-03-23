ba7509d6403bdba7c1a463c584e768f1992ff7d0
emmiiorji <emmanuelorji2013@gmail.com>
2023-03-23 23:06:54 +0100

    Fix linter errors

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -27,46 +27,48 @@ const Layout = () => {
   return (
     <div className="layout">
       <nav className="navbar navbar-light bg-light flex-column">
-          <img src={logo} alt="P.I. logo" style={{height: '2rem', width: '2rem'}} className="my-4 border border-dark rounded-circle"/>
-          <ul className="navbar-nav flex-column">
-            <li className='nav-item'>
-              <NavLink to="/app/investigators" className='nav-link'>Home</NavLink>
-            </li>
-            <li className='nav-item'>
-              <NavLink to="/app/appointments" className='nav-link'>My Appointments</NavLink>
-            </li>
-            <li className='nav-item'>
-              <NavLink to="/app/investigators/:id/reserve" className='nav-link'>Book an Appointment</NavLink>
-            </li>
-            <hr />
-            { currentUser.admin
+        <img src={logo} alt="P.I. logo" style={{ height: '2rem', width: '2rem' }} className="my-4 border border-dark rounded-circle" />
+        <ul className="navbar-nav flex-column">
+          <li className="nav-item">
+            <NavLink to="/app/investigators" className="nav-link">Home</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/app/appointments" className="nav-link">My Appointments</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/app/investigators/:id/reserve" className="nav-link">Book an Appointment</NavLink>
+          </li>
+          <hr />
+          { currentUser.admin
             && (
               <>
-                <li className='nav-item'>
-                  <NavLink to="/app/add_investigator" className='nav-link'>Add an Investigator</NavLink>
+                <li className="nav-item">
+                  <NavLink to="/app/add_investigator" className="nav-link">Add an Investigator</NavLink>
                 </li>
-                <li className='nav-item'>
-                  <NavLink to="/app/delete-investigator/:id" className='nav-link'>Delete Investigator</NavLink>
+                <li className="nav-item">
+                  <NavLink to="/app/delete-investigator/:id" className="nav-link">Delete Investigator</NavLink>
                 </li>
               </>
             )}
-            <Button onClick={handleLogout} variant="danger">Logout</Button>
+          <Button onClick={handleLogout} variant="danger">Logout</Button>
+        </ul>
+        <div>
+          <ul className="navbar-nav flex-row">
+            <li><a href="https://www.twitter.com" aria-label="Twitter" className="nav-link"><FaTwitter /></a></li>
+            <li><a href="https://www.facebook.com" aria-label="Facebook" className="nav-link"><FaFacebookF /></a></li>
+            <li><a href="https://www.instagram.com" aria-label="Instagram" className="nav-link"><TiSocialInstagram /></a></li>
+            <li><a href="https://www.vimeo.com" aria-label="Vimeo" className="nav-link"><FaVimeoV /></a></li>
+            <li><a href="https://www.pinterest.com" aria-label="Pinterest" className="nav-link"><FaPinterestP /></a></li>
           </ul>
-          <div>
-            <ul className="navbar-nav flex-row">
-              <li><a href="https://www.twitter.com" aria-label="Twitter" className='nav-link'><FaTwitter /></a></li>
-              <li><a href="https://www.facebook.com" aria-label="Facebook" className='nav-link'><FaFacebookF /></a></li>
-              <li><a href="https://www.instagram.com" aria-label="Instagram" className='nav-link'><TiSocialInstagram /></a></li>
-              <li><a href="https://www.vimeo.com" aria-label="Vimeo" className='nav-link'><FaVimeoV /></a></li>
-              <li><a href="https://www.pinterest.com" aria-label="Pinterest" className='nav-link'><FaPinterestP /></a></li>
-            </ul>
-            <footer>
-              <p className='fst-italic fw-light'><small>
+          <footer>
+            <p className="fst-italic fw-light">
+              <small>
                 &copy; 2023 Mátyás Gombos, Waris Haleem, Chris Clothier,
                 Felipe Oyarzo and Emmanuel Orji-Ihuoma
-              </small></p>
-            </footer>
-          </div>
+              </small>
+            </p>
+          </footer>
+        </div>
       </nav>
       <Outlet />
     </div>

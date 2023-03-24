8aae712d7db62e125e78a938a4c4869a2fb2a114
Chris Clothier <c.r.clothier@gmail.com>
2023-03-24 16:16:02 +0000

    Style InvestigatorForm

--- src/components/investigators/InvestigatorForm.jsx
+++ src/components/investigators/InvestigatorForm.jsx
@@ -49,18 +49,34 @@ const InvestigatorForm = () => {
   };
 
   return (
-    <div>
-      <h1>Add an Investigator</h1>
-      <hr />
-      <p>Use the following form to add a new investigator.</p>
-      <Form onSubmit={handleSubmit}>
-        <Form.Control placeholder="Name" type="text" value={state.name} required onChange={(e) => handleChange(e, 'name')} />
-        <Form.Control placeholder="Photo link" type="text" value={state.photo} required onChange={(e) => handleChange(e, 'photo')} />
-        <Form.Control placeholder="Description" type="text" value={state.description} required onChange={(e) => handleChange(e, 'description')} />
-        <Form.Control placeholder="Fee" type="number" value={state.fee} required onChange={(e) => handleChange(e, 'fee')} />
-        <Form.Control placeholder="Rating" type="number" value={state.rating} required onChange={(e) => handleChange(e, 'rating')} />
-        <Button type="submit">Submit</Button>
-      </Form>
+    <div
+      className=""
+      style={{
+        backgroundImage: 'url("https://mdbootstrap.com/img/Photos/Others/images/76.jpg")',
+        height: '100vh',
+        width: '100%',
+      }}
+    >
+      <div
+        className="d-flex flex-column justify-content-center align-items-center opacity-75"
+        style={{
+          height: '100vh',
+          backgroundColor: '#97BF0F',
+          color: 'white',
+        }}
+      >
+        <h1>Add an Investigator</h1>
+        <hr className="border border-2 w-25" />
+        <p className="mb-5">Use the following form to add a new investigator.</p>
+        <Form onSubmit={handleSubmit} className="row gap-3">
+          <Form.Control className="col rounded-pill border border-white text-white py-3 px-3" style={{ backgroundColor: '#97BF0F' }} placeholder="Name" type="text" value={state.name} required onChange={(e) => handleChange(e, 'name')} />
+          <Form.Control className="col rounded-pill border border-white text-white py-3 px-3" style={{ backgroundColor: '#97BF0F' }} placeholder="Photo link" type="text" value={state.photo} required onChange={(e) => handleChange(e, 'photo')} />
+          <Form.Control className="col rounded-pill border border-white text-white py-3 px-3" style={{ backgroundColor: '#97BF0F' }} placeholder="Description" type="text" value={state.description} required onChange={(e) => handleChange(e, 'description')} />
+          <Form.Control className="col rounded-pill border border-white text-white py-3 px-3" style={{ backgroundColor: '#97BF0F' }} placeholder="Fee" type="number" value={state.fee} required onChange={(e) => handleChange(e, 'fee')} />
+          <Form.Control className="col rounded-pill border border-white text-white py-3 px-3" style={{ backgroundColor: '#97BF0F' }} placeholder="Rating" type="number" value={state.rating} required onChange={(e) => handleChange(e, 'rating')} />
+          <Button type="submit" className="rounded-pill border border-white" style={{ backgroundColor: 'white', color: '#97BF0F', fontWeight: 'bold' }}>Submit</Button>
+        </Form>
+      </div>
     </div>
   );
 };

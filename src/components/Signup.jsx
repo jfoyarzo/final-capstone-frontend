8206a68cabe3bb613eca5577e5e414cf6f9d76b5
emmiiorji <emmanuelorji2013@gmail.com>
2023-03-24 18:27:51 +0100

    Update and style the signup component

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { Button, Form } from 'react-bootstrap';
 import Swal from 'sweetalert2';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Form } from 'react-bootstrap';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { signUpUser } from '../redux/CurrentUser/CurrentUserSlice';
 
+const theme = createTheme();
 const Signup = () => {
   const currentUser = useSelector((state) => state.userReducer);
   const [validated, setValidated] = useState(false);
@@ -39,30 +46,64 @@ const Signup = () => {
     setValidated(true);
   };
   return (
-    <Form validated={validated} id="form" onSubmit={handleSignup}>
-      <Form.Group className="mb-3">
-        <Form.Label>Name</Form.Label>
-        <Form.Control required type="text" name="name" />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>Email</Form.Label>
-        <Form.Control required type="email" name="email" />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>
-          Password (at least 8 characters and cointaining 1 capital letter, 1 symbol, and 1 number)
-        </Form.Label>
-        <Form.Control required type="password" name="password" pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$" />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>Confirm Password</Form.Label>
-        <Form.Control required type="password" name="passwordConf" />
-      </Form.Group>
-      <Button type="submit">
-        Sign up
-      </Button>
-    </Form>
+    <div
+      className=""
+      style={{
+        backgroundImage: 'url("https://drive.google.com/uc?id=1eaP9yms1pKKLtJCt0gX4tFCHtpk-URLD")',
+        height: '100vh',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        color: '#fff',
+      }}
+    >
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              paddingTop: '5rem',
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              Sign up
+            </Typography>
+            <Form validated={validated} id="form" onSubmit={handleSignup}>
+              <Form.Group className="mb-3">
+                <Form.Label>Name</Form.Label>
+                <Form.Control required type="text" name="name" />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Email</Form.Label>
+                <Form.Control required type="email" name="email" />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>
+                  Password (at least 8 characters & 1 Capital, 1 symbol, and 1 number)
+                </Form.Label>
+                <Form.Control required type="password" name="password" pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$" />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control required type="password" name="passwordConf" />
+              </Form.Group>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                style={{ backgroundColor: '#97BF0F' }}
+              >
+                Sign Up
+              </Button>
+            </Form>
+          </Box>
+        </Container>
+      </ThemeProvider>
+    </div>
   );
 };
-
 export default Signup;

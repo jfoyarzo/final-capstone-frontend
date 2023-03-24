f27f2ac9e154c3a2d96e9de66f39b80de6436278
emmiiorji <emmanuelorji2013@gmail.com>
2023-03-24 18:26:10 +0100

    Update and style login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,6 +2,13 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { Form } from 'react-bootstrap';
 import { getUser } from '../redux/CurrentUser/CurrentUserSlice';
 
 const Login = () => {
@@ -28,28 +35,61 @@ const Login = () => {
       });
     }
   }, [currentUser, navigate]);
-
   const handleLogin = (e) => {
     e.preventDefault();
     const [email, password] = document.getElementById('form').elements;
     dispatch(getUser({ email: email.value, password: password.value }));
   };
-
+  const theme = createTheme();
   return (
-    <form id="form" onSubmit={handleLogin}>
-      <label htmlFor="email">
-        Email
-        <input type="email" name="email" />
-      </label>
-      <label htmlFor="password">
-        Password
-        <input type="password" htmlFor="password" />
-      </label>
-      <button type="submit">
-        Login
-      </button>
-    </form>
+    <div
+      className=""
+      style={{
+        backgroundImage: 'url("https://drive.google.com/uc?id=1eaP9yms1pKKLtJCt0gX4tFCHtpk-URLD")',
+        height: '100vh',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        color: '#fff',
+      }}
+    >
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              paddingTop: '5rem',
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              Login
+            </Typography>
+            <form id="form" onSubmit={handleLogin}>
+              <Form.Group className="mb-3">
+                <Form.Label>Email</Form.Label>
+                <Form.Control required type="email" name="email" />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Password</Form.Label>
+                <Form.Control required type="password" name="password" />
+              </Form.Group>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                style={{ backgroundColor: '#97BF0F' }}
+              >
+                Login
+              </Button>
+            </form>
+          </Box>
+        </Container>
+      </ThemeProvider>
+    </div>
   );
 };
-
 export default Login;
